0756dc6c7b4f20a0af07bdda3917101b99ce8a3c
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-03-15 12:21:46 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'], message: "is not included in the list"}
+
+    validate :title_clickbait
+    
+    def title_clickbait
+        unless title =~ /(Won't Believe|Secret|Top \d+|Guess)/
+            errors.add(:title, "must be clickbait-y")
+        end
+    end
 end
